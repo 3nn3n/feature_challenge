4927c7a48970fba75a88eb2cc719a5123ff574b0
Nitesh Negi <nitesh_negi@outlook.com>
2026-05-08 10:20:30 +0530

    settled background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,25 @@
-import BackgroundAnimation from "@/components/backgroundAnimation";
+import Navbar from "@/components/navBar";
 import { tokens } from "@/tokens/colors";
 
 export default function Home() {
   return (
-    <>
-      <div>
-        <BackgroundAnimation />
+    <section className="relative h-[300vh] overflow-hidden bg-[#020617] flex items-center justify-center">
+      <div className="absolute inset-0">
+        {[...Array(30)].map((_, i) => (
+          <div
+            key={i}
+            className="absolute w-[2px] h-[2px] rounded-full bg-white animate-pulse"
+            style={{
+              top: `${Math.random() * 100}%`,
+              left: `${Math.random() * 100}%`,
+              opacity: Math.random(),
+            }}
+          />
+        ))}
       </div>
-    </>
+
+      <Navbar />
+
+    </section>
   );
 }
---
+++ src/components/navBar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav className='fixed top-0 left-0 z-50 md:p-9 p-3'>
+     <img src="./logo.svg" alt="nav-logo" className=' md:w-16 w-12'/>   
+    </nav>
+  )
+}
+
+export default Navbar
