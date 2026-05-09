8567ed22bba355ee32e35038add1addbb5bb2f57
Nitesh Negi <nitesh_negi@outlook.com>
2026-05-09 20:09:12 +0530

    activity api

--- src/components/activityComponent.tsx
+++ src/components/activityComponent.tsx
@@ -1,17 +1,29 @@
 import React from 'react'
 import { useCarts } from '@/hooks/useCardAPI';
 
-const ActivityComponent = () => {
+type ActivityProps = {
+  image?: number;
+  name?: string;
+  place?: string;
+};
+
+const ActivityComponent: React.FC<ActivityProps> = (
+  {
+    image,
+    name,
+    place
+  }
+) => {
   const { data, loading, error } = useCarts();
   console.log(data);
 
   return (
     <div className='mt-2 flex min-h-[52px] w-full items-center justify-between rounded-xl border border-(--color-border-primary)/30 bg-(--color-surface-card)/70 px-3 py-2 sm:px-4'>
       <div className='flex items-center justify-start gap-4'>
-        <div className='text-[10px] sm:text-xs text-(--color-text-secondary)'>AWS</div>
+        <div className='text-[10px] sm:text-xs text-(--color-text-secondary)'>{image}</div>
         <div className='text-xs sm:text-sm text-(--color-text-primary)'>
-          <div>Virtual Machine</div>
-          <div>US East 1 </div>
+          <div>{name}</div>
+          <div>{[place]}</div>
         </div>
       </div>
       <div className='text-xs sm:text-sm text-emerald-300'>Success</div>
--- src/components/recentActivityCard.tsx
+++ src/components/recentActivityCard.tsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import ActivityComponent from './activityComponent'
+import { useCarts } from '@/hooks/useCardAPI';
 
 const RecentActivityCard = () => {
+  const { data, loading, error } = useCarts();
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Failed to load carts</div>;
   return (
     <div className='w-full min-h-[260px] sm:min-h-[270px] rounded-2xl border border-(--color-border-primary)/60 bg-(--gradient-panel) p-4 sm:p-5'>
       <span className='text-base sm:text-sm font-semibold text-(--color-text-primary)'>Recent Activity</span>
-      <ActivityComponent />
-      <ActivityComponent />
-      <ActivityComponent />
+      <ActivityComponent
+      image={data?.carts[0]?.products[0].id}
+      name={data?.carts[0]?.products[0].title}
+      place='East Coast'
+      />
+      <ActivityComponent
+      image={data?.carts[26]?.products[2].id}
+      name={data?.carts[26]?.products[2].title}
+      place='Singapore'
+      />
+      <ActivityComponent
+      image={data?.carts[19]?.products[1].id}
+      name={data?.carts[19]?.products[1].title}
+      place='Australia'
+      />
       </div>
   )
 }
